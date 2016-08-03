60e2d4ac93327069377502505cc35c4c77162968
Sebastian Leu <sebastian.leu1@gmail.com>
2016-08-03 14:53:27 +0200

    Improvements for cache adapter class

--- src/FFMpegCache.php
+++ src/FFMpegCache.php
@@ -18,41 +18,51 @@ class FFMpegCache implements Cache {
   protected $cache;
 
   /**
+   * Prefix for the cache ids.
+   *
+   * @var string
+   */
+  protected $prefix;
+
+  /**
    * Constructs a CacheCollector object.
    *
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache
    *   The cache backend.
+   * @param string $prefix
+   *   Prefix used for appending to cached item identifiers.
    */
-  public function __construct(CacheBackendInterface $cache) {
+  public function __construct(CacheBackendInterface $cache, $prefix) {
     $this->cache = $cache;
+    $this->prefix = (string) $prefix;
   }
 
   /**
    * @inheritdoc
    */
   public function fetch($id) {
-    return $this->cache->get($id);
+    return $this->cache->get($this->getCid($id));
   }
 
   /**
    * @inheritdoc
    */
   public function contains($id) {
-    return !!$this->cache->get($id);
+    return !!$this->cache->get($this->getCid($id));
   }
 
   /**
    * @inheritdoc
    */
   public function save($id, $data, $lifeTime = 0) {
-    $this->cache->set($id, $data, time() + $lifeTime);
+    $this->cache->set($this->getCid($id), $data, time() + $lifeTime);
   }
 
   /**
    * @inheritdoc
    */
   public function delete($id) {
-    $this->cache->delete($id);
+    $this->cache->delete($this->getCid($id));
   }
 
   /**
@@ -60,6 +70,19 @@ class FFMpegCache implements Cache {
    */
   public function getStats() {
     return NULL;
+  }
+
+  /**
+   * Returns a prefixed cache id based on given id.
+   *
+   * @param string $id
+   *   The id string to prefix.
+   *
+   * @return string
+   *   A prefixed cache id.
+   */
+  protected function getCid($id) {
+    return "{$this->prefix}:{$id}";
   }
 
 }
